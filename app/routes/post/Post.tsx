21de79c7b39c2be6717e5ee4187193dcf0565ddd
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-01-10 22:19:08 +0100

    feat: fetch and delete a post

--- app/routes/post/Post.tsx
+++ app/routes/post/Post.tsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import type { Route } from '../+types/Post';
+import { Form } from 'react-router';
 
-export const loader = async ({ params }: Route.LoaderArgs) => {
+export const clientLoader = async ({ params }: Route.LoaderArgs) => {
     const { id } = params;
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const data = await res.json();
 
-    return { id };
+    return { id, data };
 };
 
-export const action = async () => { };
+export const clientAction = async ({ params }: Route.LoaderArgs) => {
+    const { id } = params;
+
+    await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        method: 'DELETE'
+    });
+};
 
 const Post = ({ loaderData }: Route.ComponentProps) => {
     return (
-        <div className='bg-slate-300 flex items-center justify-center h-svh w-svw'>
-            <h1 className='text-3xl font-bold'>post Id: {loaderData.id}</h1>
+        <div className='bg-slate-300 flex  justify-center h-svh w-svw'>
+            <div className='mt-10 px-10'>
+                <h1 className='text-3xl font-bold'>post Id: {loaderData.id}</h1>
+
+                <div className='bg-white p-5 mt-5'>
+                    <h2 className='text-xl font-bold'>{loaderData.data.title}</h2>
+                    <p className='text-lg'>{loaderData.data.body}</p>
+                </div>
+                <Form method='delete'>
+                    <button type='submit' className='bg-red-500 text-white p-2 mt-5'>Delete</button>
+                </Form>
+            </div>
         </div>
     )
 }
