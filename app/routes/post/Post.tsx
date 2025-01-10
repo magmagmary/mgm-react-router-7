b81bd47958cbebeac70f4812367a033d012aec90
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-01-10 22:35:31 +0100

    feat: fetcher

--- app/routes/post/Post.tsx
+++ app/routes/post/Post.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { Route } from '../+types/Post';
-import { Form } from 'react-router';
+import { Form, useFetcher } from 'react-router';
 
 export const clientLoader = async ({ params }: Route.ClientLoaderArgs) => {
     const { id } = params;
@@ -13,24 +13,36 @@ export const clientLoader = async ({ params }: Route.ClientLoaderArgs) => {
 export const clientAction = async ({ params }: Route.ClientActionArgs) => {
     const { id } = params;
 
-    await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-        method: 'DELETE'
-    });
+    try {
+        await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+            method: 'DELETE'
+        });
+
+        return { isDeleted: true };
+    } catch (error) {
+        return { isDeleted: false };
+    }
+
 };
 
 const Post = ({ loaderData }: Route.ComponentProps) => {
+    const fetcher = useFetcher();
+    const { isDeleted } = fetcher.data || {};
+
     return (
         <div className='bg-slate-300 flex  justify-center h-svh w-svw'>
             <div className='mt-10 px-10'>
                 <h1 className='text-3xl font-bold'>post Id: {loaderData.id}</h1>
 
-                <div className='bg-white p-5 mt-5'>
-                    <h2 className='text-xl font-bold'>{loaderData.data.title}</h2>
-                    <p className='text-lg'>{loaderData.data.body}</p>
-                </div>
-                <Form method='delete'>
+                {!isDeleted &&
+                    <div className='bg-white p-5 mt-5'>
+                        <h2 className='text-xl font-bold'>{loaderData.data.title}</h2>
+                        <p className='text-lg'>{loaderData.data.body}</p>
+                    </div>
+                }
+                <fetcher.Form method='delete'>
                     <button type='submit' className='bg-red-500 text-white p-2 mt-5'>Delete</button>
-                </Form>
+                </fetcher.Form>
             </div>
         </div>
     )
