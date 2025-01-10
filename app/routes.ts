4d98eb66e29dae596e7984082abef231bb59e5d3
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-01-10 21:16:20 +0100

    feat: add dynamic route

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
 	index("routes/Home.tsx"),
 	route("about", "routes/About.tsx"),
+	route("post/:id", "routes/Post.tsx"),
 ] satisfies RouteConfig;
---
+++ app/routes/Post.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import type { Route } from './+types/Post';
+
+export const loader = async ({ params }: Route.LoaderArgs) => {
+    const { id } = params;
+
+    return { id };
+};
+
+export const action = async () => { };
+
+const Post = ({ loaderData }: Route.ComponentProps) => {
+    return (
+        <div className='bg-slate-300 flex items-center justify-center h-svh w-svw'>
+            <h1 className='text-3xl font-bold'>post #{loaderData.id}</h1>
+        </div>
+    )
+}
+
+export default Post
