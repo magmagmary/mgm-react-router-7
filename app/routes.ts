9047bf8e4f13494203777774d3a9224a5c31e14a
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-01-10 21:15:59 +0100

    feat: add new route

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,6 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/Home.tsx")] satisfies RouteConfig;
+export default [
+	index("routes/Home.tsx"),
+	route("about", "routes/About.tsx"),
+] satisfies RouteConfig;
---
+++ app/routes/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='bg-slate-300 flex items-center justify-center h-svh w-svw'>
+            <h1 className='text-3xl font-bold'>About page</h1>
+        </div>
+    )
+}
+
+export default About
