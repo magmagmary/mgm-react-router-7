9a01d3f699394b0b140efbf2811469946de8b9cb
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-01-10 21:35:41 +0100

    feat: refactor structure

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-	index("routes/Home.tsx"),
-	route("about", "routes/About.tsx"),
-	route("post/:id", "routes/Post.tsx"),
+	index("routes/home/Home.tsx"),
+	route("about", "routes/about/About.tsx"),
+	route("post/:id", "routes/post/Post.tsx"),
 ] satisfies RouteConfig;
--- app/routes/post/Post.tsx
+++ app/routes/post/Post.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import type { Route } from './+types/Post';
+import type { Route } from '../+types/Post';
 
 export const loader = async ({ params }: Route.LoaderArgs) => {
     const { id } = params;
@@ -12,7 +12,7 @@ export const action = async () => { };
 const Post = ({ loaderData }: Route.ComponentProps) => {
     return (
         <div className='bg-slate-300 flex items-center justify-center h-svh w-svw'>
-            <h1 className='text-3xl font-bold'>post #{loaderData.id}</h1>
+            <h1 className='text-3xl font-bold'>post Id: {loaderData.id}</h1>
         </div>
     )
 }
