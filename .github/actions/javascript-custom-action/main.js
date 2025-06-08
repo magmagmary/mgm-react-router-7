dbc6f7d3eb3ae5b5c43fe7060bd341d044a26353
Maryam Esmaeilzadeh <maryam@Maryams-MacBook-Pro.local>
2025-06-08 14:21:31 +0200

    feat: s3 deployment

--- .github/actions/javascript-custom-action/main.js
+++ .github/actions/javascript-custom-action/main.js
@@ -1,5 +1,31 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
 
 (function main() {
+    // Get inputs from the action
+    const bucketName = core.getInput('bucket' , {
+        required: true
+    })
+    const bucketRegion = core.getInput('bucket-region', {
+        required: true,
+    });
+    const distFolder = core.getInput('dist-folder', {
+        required: true  
+    });
+
+
+    exec.exec('aws s3 sync', [
+        distFolder,
+        `s3://${bucketName}`,
+        '--region',
+        bucketRegion,
+        '--delete',
+    ]).then(() => {
+        core.info(`Successfully synced ${distFolder} to s3://${bucketName}`);
+    }).catch((error) => {
+        core.setFailed(`Failed to sync to S3: ${error.message}`);
+    });
+
+
     core.notice('This is a custom JavaScript action running in Node.js 22.');
 })();
